e017b31a9785393b8c39d33b9e64cb2370414a93
zjc <zjcjim@outlook.com>
2024-07-14 00:10:26 +0800

    perfect run. magic number 0.05

--- serial_test.py
+++ serial_test.py
@@ -1,7 +1,5 @@
 # data structure: "(int)MA(int)MB(int)MC(int)MD(float)SA(float)SB"
 
-
-from cgitb import reset
 import struct
 import time
 import serial
@@ -10,8 +8,6 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 
 if __name__ == "__main__":
     while True:
-        reset_input_buffer()
-        reset_output_buffer()
         motor1 = 10
         motor2 = 20
         motor3 = 30
@@ -20,10 +16,10 @@ if __name__ == "__main__":
         servo1 = -50
         servo2 = 30
 
-        send = str(motor1) + " " + str(motor2) + " " + str(motor3) + " " + str(motor4) + " " + str(servo1) + " " + str(servo2) 
+        send = str(motor1) + " " + str(motor2) + " " + str(motor3) + " " + str(motor4) + " " + str(servo1) + " " + str(servo2) + "\n"
         ser.write(send.encode("utf_8"))
-        time.sleep(0.05)
-        ser.write("\n".encode("utf_8"))
+        
         data = ser.readline()
         rtn = data.decode("utf-8").replace('\n','')
         print(rtn)
+        time.sleep(0.05)
