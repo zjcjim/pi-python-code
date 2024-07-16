0dfb6610ea2b2db2cce2db8d3ede8885761d8e34
张锦晨 <zjcjim@outlook.com>
2024-07-17 00:05:37 +0800

    chore: Adjust motor speed smoothing function coefficients and servo control

--- app.py
+++ app.py
@@ -292,7 +292,7 @@ def position_event():
                 #                        1 * target_lost_counter + 12 * fast_side_coefficient], 
                 #                        20)
                 # motor_speeds = [0, 0, int(1 * target_lost_counter + 20 * slow_side_coefficient), (4 * target_lost_counter + 35 * fast_side_coefficient)]
-                motor_speeds = [0, 0, 0, 120]
+                motor_speeds = [40, 0, 0, 130]
             else:
                 # turn left
                 # motor_speed_smoothing([0,
@@ -301,7 +301,7 @@ def position_event():
                 #                        1 * target_lost_counter + 10 * slow_side_coefficient], 
                 #                        20)
                 # motor_speeds = [0, 0, int(1 * target_lost_counter + 20 * fast_side_coefficient), (4 * target_lost_counter + 30 * slow_side_coefficient)]
-                motor_speeds = [0, 0, 120, 0]
+                motor_speeds = [0, 40, 130, 0]
             target_lost_counter += 1
             target_found_counter = 0
         elif target_found_counter < 6 and is_target_lost == True:
