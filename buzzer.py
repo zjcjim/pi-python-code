aad96bfec648ffc1df8046548ef45f31b7e41889
张锦晨 <zjcjim@outlook.com>
2024-07-18 02:44:58 +0800

    chore: Update buzzer.py with corrected frequency for E6 and C6 sharp

--- buzzer.py
+++ buzzer.py
@@ -25,7 +25,7 @@ frequencyC6sp = 1135
 frequencyD6 = 1175
 frequencyD6s = 1245
 frequencyE6 = 1319
-frequencyE6sp = 1352
+frequencyE6sp = 1340
 frequencyF6 = 1397
 frequencyF6s = 1480
 stop = 0
@@ -52,29 +52,29 @@ def play_note_sequence():
     # play_tone(frequencyA5, 2)
     # play_tone(frequencyB5, 2)
     
-    # play_tone(frequencyB5, 0.2)
-    # play_tone(frequencyA5s, 0.2)
-    # play_tone(frequencyF5s, 0.2)
-    # play_tone(frequencyF5s, 0.2)
-    # play_tone(frequencyG5s, 0.1)
-    # play_tone(frequencyG5s, 0.3)
-    # play_tone(frequencyF5s, 0.2)
-    # play_tone(frequencyF5s, 0.2)
-    # play_tone(frequencyG5s, 0.1)
-    # play_tone(frequencyG5s, 0.3)
-    # play_tone(frequencyF5s, 0.2)
-    # play_tone(frequencyF5s, 0.2)
-    # play_tone(frequencyG5s, 0.1)
-    # play_tone(frequencyG5s, 0.3)
-    # play_tone(frequencyG5s, 0.2)
-    # play_tone(frequencyG5s, 0.2)
-    # play_tone(frequencyA5s, 0.2)
-    # play_tone(frequencyB5, 0.2)
-    # play_tone(frequencyC6s, 0.2)
-    play_tone(frequencyA5s, 2)
-    play_tone(frequencyF5s, 2)
-    play_tone(frequencyE6, 2)
-    play_tone(frequencyC6sp, 2)
+    play_tone(frequencyB5, 0.2)
+    play_tone(frequencyA5s, 0.2)
+    play_tone(frequencyF5s, 0.2)
+    play_tone(frequencyF5s, 0.2)
+    play_tone(frequencyG5s, 0.1)
+    play_tone(frequencyG5s, 0.3)
+    play_tone(frequencyF5s, 0.2)
+    play_tone(frequencyF5s, 0.2)
+    play_tone(frequencyG5s, 0.1)
+    play_tone(frequencyG5s, 0.3)
+    play_tone(frequencyF5s, 0.2)
+    play_tone(frequencyF5s, 0.2)
+    play_tone(frequencyG5s, 0.1)
+    play_tone(frequencyG5s, 0.3)
+    play_tone(frequencyG5s, 0.2)
+    play_tone(frequencyG5s, 0.2)
+    play_tone(frequencyA5s, 0.2)
+    play_tone(frequencyB5, 0.2)
+    play_tone(frequencyC6s, 0.2)
+    play_tone(frequencyA5s, 0.4)#844
+    play_tone(frequencyF5s, 0.4)#643/683
+    play_tone(frequencyE6sp, 0.4)#1150/1119
+    play_tone(frequencyC6sp, 0.4)#983
     play_tone(frequencyC6sp, 0.3)
     play_tone(frequencyC6sp, 0.3)
     play_tone(frequencyD6s, 0.2)
@@ -134,7 +134,7 @@ def play_note_sequence():
     play_tone(frequencyC6s, 0.2)
     play_tone(frequencyA5s, 0.4)
     play_tone(frequencyF5s, 0.4)
-    play_tone(frequencyE6, 0.4)
+    play_tone(frequencyE6sp, 0.4)
     play_tone(frequencyC6sp, 0.4)
     play_tone(frequencyC6sp, 0.3)
     play_tone(frequencyC6sp, 0.3)
