f8f81965781ecaeff3fc1e1a587d53876936d428
张锦晨 <zjcjim@outlook.com>
2024-07-17 15:44:12 +0800

    chore: Update laser control logic for continuous operation

--- laser.py
+++ laser.py
@@ -5,8 +5,13 @@ laser_pin = 14
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(laser_pin, GPIO.OUT)
 
-for i in range(10):
-    GPIO.output(laser_pin, GPIO.HIGH)
-    time.sleep(0.5)
-    GPIO.output(laser_pin, GPIO.LOW)
-    time.sleep(0.5)
+while True:
+    try:
+        GPIO.output(laser_pin, GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(laser_pin, GPIO.LOW)
+        time.sleep(0.2)
+    except KeyboardInterrupt:
+        GPIO.output(laser_pin, GPIO.LOW)
+        GPIO.cleanup()
+        break
