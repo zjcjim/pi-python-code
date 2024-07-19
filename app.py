37aa1a9e935fbc5b928b8ef739ae5ecd7d65e8a0
张锦晨 <zjcjim@outlook.com>
2024-07-19 16:15:21 +0800

    chore: Update motor speeds and search pattern for better control and responsiveness

--- app.py
+++ app.py
@@ -305,10 +305,10 @@ def motor_control(previous_angle_x, x_direction, is_target_lost=False):
             if previous_angle_x > 90:
                 # turn left
                 # add a coefficent
-                motor_speed_smoothing([30, speed_diff * 1.2 + 150, speed_diff * 1.2 + 150, 30], 45)
+                motor_speed_smoothing([20, speed_diff * 1.1 + 120, speed_diff * 1.1 + 120, 20], 45)
             else:
                 # turn right
-                motor_speed_smoothing([speed_diff + 150, 40, 40, speed_diff + 150], 45)
+                motor_speed_smoothing([speed_diff + 120, 20, 20, speed_diff + 120], 45)
         else:
             # go straight
             motor_speed_smoothing([120, 120, 120, 120], 60)
